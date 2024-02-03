93a5008f925df843152e6bbfbac1a36d42b511ed
sidsidu <47473176+sidsidu@users.noreply.github.com>
2024-02-03 22:11:08 +0530

    Add files via upload

---
+++ yo.js
@@ -0,0 +1,12 @@
+function yes()
+{
+    var img = document.getElementById('image');
+    img.src = 'yess.gif';
+    var text1 = document.getElementById('start').innerText='YAAAAYYYYYYYYYYYYYYYYYYYYYY'
+}
+function no()
+{
+    var img = document.getElementById('image');
+    img.src = 'no.gif';
+    var text1 = document.getElementById('start').innerText='NOOOOOOOOOOOOOOOOOOOOOOOOOOOO :('
+}
